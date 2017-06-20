8d14c1613755e0173853eb8b2bfc63f700ad6e51
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-20 18:58:19 +0100

    change, for naming consistency, AppComponent selector from 'mk-root' to 'mk-app'

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mk-root',
+  selector: 'mk-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
