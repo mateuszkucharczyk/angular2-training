7b44ac89156301022782629e0499fae638fdc4a9
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-20 22:54:39 +0100

    provide heroes through HeroService

--- tour-of-heroes/src/app/heros-list/heros-list.component.ts
+++ tour-of-heroes/src/app/heros-list/heros-list.component.ts
@@ -1,29 +1,20 @@
+import { HeroService } from './../hero.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Hero } from '../hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
   selector: 'mk-heros-list',
   templateUrl: './heros-list.component.html',
-  styleUrls: ['./heros-list.component.scss']
+  styleUrls: ['./heros-list.component.scss'],
+  providers: [HeroService]
 })
 export class HerosListComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   @Output() heroSelected = new EventEmitter<Hero>()
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+    this.heroes = heroService.getHeroes();
+  }
 
   ngOnInit() {
   }
