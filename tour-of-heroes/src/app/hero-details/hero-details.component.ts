43d1948419353411ff0e351ad5ab864f37bf79ac
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-20 19:38:26 +0100

    Bind, one-way, HeroDetailsComponent properties to HTML template

--- tour-of-heroes/src/app/hero-details/hero-details.component.ts
+++ tour-of-heroes/src/app/hero-details/hero-details.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
+class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'mk-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.scss']
 })
 export class HeroDetailsComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstrom'
+  };
 
   constructor() { }
 
