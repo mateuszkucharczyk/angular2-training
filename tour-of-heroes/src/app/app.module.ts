ae9d31a70778590d4b5dc70c4ffc9b1e460e79b2
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-20 19:23:49 +0100

    Angular CLI: generate HeroDetailsComponent
    ng g c hero-details

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule
