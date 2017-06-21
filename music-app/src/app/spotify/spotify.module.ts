6cb131b6625e01ac208c0d7303ee9e563f8e2533
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-21 10:29:29 +0100

    Use two way binding instead local template variable

--- music-app/src/app/spotify/spotify.module.ts
+++ music-app/src/app/spotify/spotify.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   declarations: [SearchComponent]
 })
