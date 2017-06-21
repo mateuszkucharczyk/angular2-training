6d5d28111af4e431d94382d206dc5b1f5c032452
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-21 10:03:00 +0100

    Querry shows by title on 'Find' button click. Use tamplate local variable to store DOM element.

--- music-app/src/app/spotify/search/search.component.ts
+++ music-app/src/app/spotify/search/search.component.ts
@@ -15,20 +15,25 @@ import 'rxjs/add/operator/map'
 })
 export class SearchComponent implements OnInit {
   shows: Show[] = [];
-  constructor(http: Http) {
-    const apiUrl = 'https://api.tvmaze.com/search/shows?q=flash'; // 'https://api.spotify.com/v1/search?type=album&q=batman';
+  query = 'batman';
 
+  constructor(private http: Http) {
+    this.search(this.query);
+  }
+
+  search(query: string) {
+    // notice url is enclosed with ` not '
+    const apiUrl = `https://api.tvmaze.com/search/shows?q=${query}`; // 'https://api.spotify.com/v1/search?type=album&q=batman';
 
     // subscribe() first - get() does not send GET request until anything is subscribed to handle response
     // handle response via callback function
-    http.get(apiUrl)
+    this.http.get(apiUrl)
       .subscribe((resp) => this.shows = (resp.json() as ShowData[]).map(data => data.show));
     // alternative syntax: map({show} => show)
 
     // This is how we should do it.
     // Handle response by RxJS stream instead callbacks
-    http.get(apiUrl).map((resp) => (resp.json() as ShowData[]).map(data => data.show));
-
+    this.http.get(apiUrl).map((resp) => (resp.json() as ShowData[]).map(data => data.show));
   }
 
   ngOnInit() {
