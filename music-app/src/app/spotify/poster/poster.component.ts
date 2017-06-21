3b04d58b309a4b3e603540270224d941400407be
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-21 11:17:40 +0100

    make size of a poster configrable using CSS 'lg'/'md' syntax

--- music-app/src/app/spotify/poster/poster.component.ts
+++ music-app/src/app/spotify/poster/poster.component.ts
@@ -1,6 +1,8 @@
 import { Show } from './../search/spotify.models';
 import { Component, OnInit, Input } from '@angular/core';
 
+export type PosterSize = 'md' | 'lg';
+
 @Component({
   selector: 'ma-poster',
   templateUrl: './poster.component.html',
@@ -8,9 +10,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PosterComponent implements OnInit {
   @Input() tvShow: Show;
-  constructor() { }
+  @Input() size: PosterSize;
+  imgUrl: string;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.size === 'lg'
+      ? this.imgUrl = this.tvShow.image.original
+      : this.imgUrl = this.tvShow.image.medium;
+
+    // if (this.size === 'lg') {
+    //   this.imgUrl = this.tvShow.image.original;
+    // } else {
+    //   this.imgUrl = this.tvShow.image.medium;
+    // }
   }
 
 }
