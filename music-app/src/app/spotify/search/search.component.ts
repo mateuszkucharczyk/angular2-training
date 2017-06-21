2aea0549fc003367fa8a6059fa194191f862e6c8
Mateusz Kucharczyk <mateuszkucharczyk@protonmail.com>
2017-06-21 09:41:46 +0100

    Handle GET response using RxJS stream.map()

--- music-app/src/app/spotify/search/search.component.ts
+++ music-app/src/app/spotify/search/search.component.ts
@@ -3,6 +3,10 @@ import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 
 
+// RxJS stream does not have all operators imported at once because there is too many of them.
+// Pick ones you need
+import 'rxjs/add/operator/map'
+
 // ng g c spotify/search
 @Component({
   selector: 'ma-search',
@@ -14,12 +18,17 @@ export class SearchComponent implements OnInit {
   constructor(http: Http) {
     const apiUrl = 'https://api.tvmaze.com/search/shows?q=flash'; // 'https://api.spotify.com/v1/search?type=album&q=batman';
 
-    // subscribe starts RxJS stream
+
+    // subscribe() first - get() does not send GET request until anything is subscribed to handle response
+    // handle response via callback function
     http.get(apiUrl)
       .subscribe((resp) => this.shows = (resp.json() as ShowData[]).map(data => data.show));
+    // alternative syntax: map({show} => show)
 
-    // alternative
-    // .subscribe((resp) => this.shows = (resp.json() as ShowData[]).map({show} => show));
+    // This is how we should do it.
+    // Handle response by RxJS stream instead callbacks
+    http.get(apiUrl).map((resp) => (resp.json() as ShowData[]).map(data => data.show));
+
   }
 
   ngOnInit() {
